fcae9301d5de26422248336b1b064fe8e7c899b6
Philip Sinnott <philipsinnotts@gmail.com>
2021-09-23 17:56:13 +0200

    HTTP methods etc.

---
+++ BookAPIDemo/Repositories/BookRepository.cs
@@ -0,0 +1,51 @@
+﻿using BookAPIDemo.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookAPIDemo.Repositories
+{
+    public class BookRepository : IBookRepository
+    {
+        private readonly BookContext _context;
+
+        public BookRepository(BookContext context)
+        {
+            _context = context;
+        }
+        public async Task<Book> Create(Book book)
+        {
+            _context.Books.Add(book);
+            await _context.SaveChangesAsync();
+            return book;
+        }
+
+        public async Task Delete(int id)
+        {
+            // Find specific book through PK (ID)
+            var bookToDelete = await _context.Books.FindAsync(id);
+            _context.Remove(bookToDelete);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<Book>> Get()
+        {
+            // Return all books async as a List<Book>
+            return await _context.Books.ToListAsync();
+        }
+
+        public async Task<Book> Get(int id)
+        {
+            // Return a single book
+            return await _context.Books.FindAsync(id);
+        }
+
+        public async Task Update(Book book)
+        {
+            _context.Entry(book).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ BookAPIDemo/Repositories/IBookRepository.cs
@@ -0,0 +1,18 @@
+﻿using BookAPIDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookAPIDemo.Repositories
+{
+    // Describes operations that can be performed on the database
+    public interface IBookRepository
+    {
+        Task<IEnumerable<Book>> Get();
+        Task<Book> Get(int id);
+        Task<Book> Create(Book book);
+        Task Update(Book book);
+        Task Delete(int id);
+    }
+}
