218a37fa9735e4156bb6d23aa392757d1af028b8
Philip Sinnott <philipsinnotts@gmail.com>
2021-09-24 17:39:57 +0200

    Controller + other st0ff

---
+++ BookAPIDemo/Controllers/BooksController.cs
@@ -0,0 +1,68 @@
+﻿using BookAPIDemo.Models;
+using BookAPIDemo.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookAPIDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly IBookRepository _bookRepository;
+
+        public BooksController(IBookRepository bookRepository)
+        {
+            _bookRepository = bookRepository;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Book>> GetBooks()
+        {
+            // ASP.NET will convert the Book object to json before returning it to the caller
+            return await _bookRepository.Get();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Book>> GetBook(int id)
+        {
+            return await _bookRepository.Get(id);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Book>> PostBooks([FromBody] Book book)
+        {
+            // Modelbinding process: ASP.NET will convert json in the request payload into a Book object
+            var newBook = await _bookRepository.Create(book);
+            return CreatedAtAction(nameof(GetBooks), new { id = newBook.ID }, newBook);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> PutBooks(int id, [FromBody] Book book)
+        {
+            if (id != book.ID)
+            {
+                return BadRequest(); // returns 400 value indicating bad request
+            }
+            await _bookRepository.Update(book);
+            
+            return NoContent(); // Return 204 status code to show the status has successfully been processed but that there is no data
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var bookToDelete = await _bookRepository.Get(id);
+            if (bookToDelete == null)
+            {
+                return NotFound();
+            }
+            await _bookRepository.Delete(bookToDelete.ID);
+            return NoContent();
+        }
+    }
+}
